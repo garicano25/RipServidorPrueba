22db3547e0a14ca4c4a3d7d30279006f3103e0bc
garicano25 <garicano25@gmail.com>
2024-09-18 11:45:34 -0600

    Update reportequimicos.blade.php

--- resources/views/reportes/parametros/reportequimicos.blade.php
+++ resources/views/reportes/parametros/reportequimicos.blade.php
@@ -2282,4 +2282,4 @@
 	var areas_poe = <?php echo json_encode($areas_poe); ?>;
 </script>
 <script src="/js_sitio/html2canvas.js"></script>
-<script src="/js_sitio/reportes/reportequimicos.js?v=8.0"></script>
+<script src="/js_sitio/reportes/reportequimicos.js?v=9.0"></script>
