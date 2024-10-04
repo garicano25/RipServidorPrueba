3e15e20d2e59f34c2412adf3bd3e141a9894e8ae
garicano25 <garicano25@gmail.com>
2024-10-04 10:40:43 -0600

    BIBLIOTECA (CENTRO DE INFORMACION) - FINALIZADO

---
+++ app/modelos/catalogos/centroInformacionModel.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\modelos\catalogos;
+
+use Illuminate\Database\Eloquent\Model;
+
+class centroInformacionModel extends Model
+{
+    //
+    protected $primaryKey = 'ID_CENTRO_INFORMACION';
+    protected $table = 'centroInformacion';
+    protected $fillable = [
+        'TITULO',
+        'DESCRIPCION',
+        'RUTA_DOCUMENTO',
+        'RUTA_LINK',
+    ];
+}
