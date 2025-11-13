a33abdebdc2c5534ae19ce24aed01aeaff928944
andres69542 <andres69542@gmail.com>
2025-11-13 14:02:09 -0600

    Update informesrec.blade.php

--- resources/views/catalogos/recsensorial/informesrec.blade.php
+++ resources/views/catalogos/recsensorial/informesrec.blade.php
@@ -22,7 +22,7 @@
         </div>
 
 
-        @if(auth()->user()->hasRoles(['Superusuario', 'Administrador', 'Coordinador']))
+        <!-- @if(auth()->user()->hasRoles(['Superusuario', 'Administrador', 'Coordinador']))
         <h4 class="text-white card-title" style="float: left; margin: 0px; padding: 0px;">Informes y entregables &nbsp;&nbsp;</h4>
         <select class="custom-select" style="float: left; width: 340px; height: 24px; margin: 0px; padding: 2px 4px; font-weight: 550; background: #FFF;" id="select_tiporeportes" onchange="mostrar_reporte(this.value);" disabled>
           <option value="">Seleccione</option>
@@ -34,8 +34,46 @@
 
         <button class="btn btn-light" style="margin-left: 50%;" type="button" id="btnPoeProyecto" data-toggle="tooltip" title="Población Ocupacionalmente Expuesta" disabled> <i class="fa fa-users" aria-hidden="true"></i> POE Proyecto</button>
         <button class="btn btn-light" style="margin-left: 1%;" type="button" id="btnMatriz" data-toggle="tooltip" title="Matriz de Exposición Laboral" disabled> <i class="fa fa-table" aria-hidden="true"></i> MEL Proyecto</button>
+        <button class="btn btn-light" style="margin-left: 1%;" type="button" id="btnmatrizreco" data-toggle="tooltip" title="Matriz de recomendaciones" disabled> <i class="fa fa-table" aria-hidden="true"></i> MR Proyecto</button> -->
 
-        <button class="btn btn-light" style="margin-left: 1%;" type="button" id="btnmatrizreco" data-toggle="tooltip" title="Matriz de recomendaciones" disabled> <i class="fa fa-table" aria-hidden="true"></i> MR Proyecto</button>
+        <div style="display: flex; align-items: center; justify-content: space-between; width: 100%;">
+
+          <!-- IZQUIERDA: Título + Select -->
+          <div style="display: flex; align-items: center; gap: 15px;">
+            @if(auth()->user()->hasRoles(['Superusuario', 'Administrador', 'Coordinador']))
+            <h4 class="text-white card-title" style="margin: 0;">
+              Informes y entregables
+            </h4>
+
+            <select class="custom-select"
+              style="width: 340px; height: 24px; padding: 2px 4px; font-weight: 550; background: #FFF;"
+              id="select_tiporeportes"
+              onchange="mostrar_reporte(this.value);" disabled>
+              <option value="">Seleccione</option>
+            </select>
+            @endif
+          </div>
+
+          <!-- DERECHA: Botones -->
+          <div style="display: flex; align-items: center; gap: 10px;">
+            <button class="btn btn-light" type="button" id="btnPoeProyecto"
+              data-toggle="tooltip" title="Población Ocupacionalmente Expuesta" disabled>
+              <i class="fa fa-users"></i> POE Proyecto
+            </button>
+
+            <button class="btn btn-light" type="button" id="btnMatriz"
+              data-toggle="tooltip" title="Matriz de Exposición Laboral" disabled>
+              <i class="fa fa-table"></i> MEL Proyecto
+            </button>
+
+            <button class="btn btn-light" type="button" id="btnmatrizreco"
+              data-toggle="tooltip" title="Matriz de recomendaciones" disabled>
+              <i class="fa fa-table"></i> MR Proyecto
+            </button>
+          </div>
+        </div>
+
+
 
       </div>
       <div class="card-body" style="border: 0px #f00 solid; min-height: 856px!important;" id="estructura_reporte">
